061e98f394d22d7a30172b1c99eec1004a6199e8
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-09-17 15:47:19 +0400

    feat: finish style

---
+++ audio-player/script.js
@@ -0,0 +1,8 @@
+let play = document.querySelector('.play');
+let audio = document.querySelector('audio');
+let onOff = false;
+
+play.addEventListener('click', function () {
+    
+audio.play();
+})
