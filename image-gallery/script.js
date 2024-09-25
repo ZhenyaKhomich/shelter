9d417a9792df0ccc1cfa33607eaeb5b0c6e11c2d
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-09-25 16:09:37 +0400

    feat: add modal window

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,7 +1,45 @@
 const formSearch = document.querySelector(".formSearch");
 const searchArea = document.querySelector(".searchArea");
 const butSearch = document.querySelector(".butSearch");
+const butRemoveSearch = document.querySelector(".krestLup");
+const wrap = document.querySelector(".wrap");
+const wrapModal = document.querySelector(".wrapModal");
+const images = document.querySelector(".images");
+const imgs = document.querySelectorAll(".imgGen");
+
 let ask = '';
+
+window.addEventListener('DOMContentLoaded', function() {
+  console.log(wrap);
+  wrap.style.display = 'none';
+  wrapModal.style.display = 'none';
+
+  function searchPhotoLoad () {
+    let k = fetch (`https://api.unsplash.com/photos/random?count=6&client_id=n_tIVduhSi3wQx58WdnfrXOF1zw-0wOzcoYCIQDSN58`) 
+    .then((response) => response.json())
+    .then((photos)=> createPhoto(photos));
+  }
+
+  searchPhotoLoad ();
+})
+
+images.addEventListener('click', function () {
+  const wrap = document.querySelector('.wrap');
+  const wrapModal = document.querySelector('.wrapModal');
+  const modal = document.querySelector('.modal');
+  if(event.target.classList == 'imgGen') {
+    let currentImg = event.target;
+    currentImg.setAttribute('width', 'auto');
+    currentImg.setAttribute('height', 'auto');
+    console.log(currentImg.width);
+    modal.prepend(currentImg);
+
+    wrap.style.display = '';
+    wrapModal.style.display = '';
+
+  }
+})
+
 
 formSearch.addEventListener('submit', function () {
   event.preventDefault()
@@ -15,15 +53,16 @@ formSearch.addEventListener('keypress', function () {
   if (event.keyCode == 13) {}
 })
 
+butRemoveSearch.addEventListener('click', function () {
+  searchArea.value = '';
+  
+  console.log(1);
+})
 
   function searchPhoto (ask) {
-    console.log(ask);
-      let k = fetch (`https://api.unsplash.com/search/photos?query=${ask}&per_page=30&client_id=n_tIVduhSi3wQx58WdnfrXOF1zw-0wOzcoYCIQDSN58`)
-      .then((response) => response.json())
-      .then((a)=> createPhoto(a.results));
-      console.log(k)
-      k = '';
-      ask = '';
+    let k = fetch (`https://api.unsplash.com/search/photos?query=${ask}&per_page=30&client_id=n_tIVduhSi3wQx58WdnfrXOF1zw-0wOzcoYCIQDSN58`)
+    .then((response) => response.json())
+    .then((a)=> createPhoto(a.results));
   }
 
   function createPhoto (obj) {
@@ -37,21 +76,10 @@ formSearch.addEventListener('keypress', function () {
       img.classList.add('imgGen');
       img.alt = 'picture';
 
-     img.height = 400;
-     img.width = 400;
+      img.height = 340;
+      img.width = 340;
 
-    // if(img.height > 300) {
-    //   imagesCont.append(img);
-    // } else {
-      
-    // }
-imagesCont.prepend(img);
-      
-
+      imagesCont.prepend(img);
     })
     ask = '';
   }
-
-  
-
-  console.log(ask);
