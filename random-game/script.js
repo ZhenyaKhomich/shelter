4f4c8a25b545958ca767f43e1da72f4c17c6ecc9
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-10-01 16:28:16 +0400

    feat: add modal window

---
+++ random-game/script.js
@@ -0,0 +1,7 @@
+const darkModal = document.querySelector('.darkModal');
+const wrapModal = document.querySelector('.wrapModal');
+
+document.addEventListener('DOMContentLoaded', function () {
+darkModal.style.display = 'none';
+wrapModal.style.display = 'none';
+})
